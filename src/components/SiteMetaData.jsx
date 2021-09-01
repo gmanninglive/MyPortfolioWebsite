35b32aa08380eae6d57d24caaf9f62a066bbcc71
gmanninglive <gmanning@live.co.uk>
2021-09-02 00:14:35 +0100

    accessiblity for 200% zoom mobile

--- src/components/SiteMetaData.jsx
+++ src/components/SiteMetaData.jsx
@@ -30,7 +30,6 @@ const SiteMetadata = ({ title, image, description}) => {
       titleTemplate={`%s | ${description}`}
     >
       <html lang={locale} />
-      <meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1.0, user-scalable=no" />
       <meta name="description" content={description} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
